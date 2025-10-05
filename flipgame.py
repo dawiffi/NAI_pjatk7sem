2e87a11089c772a8d68431f421b7236835159e79
Kacper Pach <kapsi1234@gmail.com>
2025-10-05 21:19:31 +0200

    adde all of the player logic

--- flipgame.py
+++ flipgame.py
@@ -1,6 +1,6 @@
 import random
 
-def generate_possible_next_moves(s: str):
+def generate_possible_next_moves(s: str) -> list:
     """
     Generate all possible states of the string after one valid move.
     A move is flipping two consecutive "++" into "--".
@@ -30,26 +30,52 @@ def generate_board(lenght : int) -> str:
     '''
     board_string = ''
     while True:
-        board_string = ''.join(random.choices(['+', '-'], k=lenght) )
+        board_string = ''.join(random.choices(['+', '-'], k=lenght))
         if board_string.count("++") > 2:
             return board_string
+            
+def get_player_move(board_state: str, possible_moves: list) -> str:
+    ''' 
+    asks the player for the move and returns the board state after
+
+    Args:
+        board_state (str): current board state
+        possible_moves (list): list of moves the player can make in this turn 
+
+    Returns:
+        str: board state after the player made his move
+    '''
+
+    # display user optiones 
+    print("Possible next moves:")
+    for i, move in enumerate(possible_moves):
+        print(f"{i} - [{move}]")
+
+    while True:
+        player_input = -1
+        try:
+            player_input = int(input("Enter the number corresponding to your choice: "))
+        except ValueError:
+            print("Invalid value. Try a number")
+            continue
+
+        if  0 <= player_input < len(possible_moves):
+            print("Valid move!")
+            return possible_moves[player_input]
+        else:
+            print("Invalid move.") 
+
 
 if __name__ == "__main__":
     board_state = generate_board(10)
-
+    
     while True:
-        posible_moves = generate_possible_next_moves(board_state)
+        print(board_state)
+        possible_moves = generate_possible_next_moves(board_state)
         print("Game board:", board_state)
-        print("Possible next moves:", posible_moves, not posible_moves)
-        if not posible_moves:
+        if not possible_moves:
             print("Player you won!")
             break
-        
-        player_input = input("enter your move (e.g., '++' to '--'): ")
-        if player_input in posible_moves:
-            print("Valid move!")
-            board_state = player_input
-        else:
-            print("Invalid move.")
-            continue 
+        board_state = get_player_move(board_state, possible_moves)
+
     print("Game over.")
