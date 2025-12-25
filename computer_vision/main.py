469068735dc9379bfd010b5b063b33fdc4a513dc
Kacper Pach <kapsi1234@gmail.com>
2025-12-25 18:42:10 +0100

    final test

--- computer_vision/main.py
+++ computer_vision/main.py
@@ -5,25 +5,19 @@ import cv2
 cap = cv2.VideoCapture("ad.mp4")
 videoPlaying = True 
 
-while(1):
-    if videoPlaying:
-        ret, frame = cap.read()
-    if cv2.waitKey(1) == ord('q') or ret==False :
-       cap.release()
-       cv2.destroyAllWindows()
-       break
-    if cv2.waitKey(1) == ord('p'):
-       videoPlaying = not videoPlaying
-    cv2.imshow('frame',frame)
-
-
-capCam = cv2.VideoCapture(0)
+capCam = cv2.VideoCapture("vide.mp4")
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-while True:
+def play_video(cap, videoPlaying):
+    if videoPlaying:
+        ret, frame = cap.read()
+        cv2.imshow('Reklama',frame)
+
+def capture_camera(capCam):
     ret, frame = capCam.read()
+    eyes_total = 0
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -32,10 +26,20 @@ while True:
         roi_gray = gray[y:y+w, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+        eyes_total += len(eyes)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+    cv2.imshow('camera', frame)
+    return eyes_total
 
-    cv2.imshow('frame_2', frame)
+     
+while True:
+    play_video(cap, videoPlaying)
+    eyes_total = capture_camera(capCam)
+    if eyes_total < 2:
+        videoPlaying = False
+    elif eyes_total >= 2:
+        videoPlaying = True
 
     if cv2.waitKey(1) == ord('q'):
         break
